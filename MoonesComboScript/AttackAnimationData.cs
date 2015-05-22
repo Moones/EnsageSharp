2f8e8bb0338ef3d3a461d055072de703a39cd639
git <moones@email.cz>
2015-05-22 17:08:28 +0200

    Combo Script rework

---
+++ MoonesComboScript/AttackAnimationData.cs
@@ -0,0 +1,101 @@
+﻿#region
+
+using Ensage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace MoonesComboScript
+{
+
+    public class AttackAnimationData
+    {
+        public string UnitName;
+        public ClassId UnitClassId;
+        public double AttackRate;
+        public double AttackPoint;
+        public double AttackBackswing;
+        public int ProjectileSpeed;
+        public double TurnRate;
+        private static double moveTime = 0;
+        private static double endTime = 0;
+        private static bool canMove = false;
+
+        public AttackAnimationData() { }
+
+        public AttackAnimationData(string unitName,
+            ClassId unitClassId,
+            double attackRate,
+            double attackPoint,
+            double attackBackswing,
+            int projectileSpeed,
+            double turnRate)
+        {
+            UnitName = unitName;
+            UnitClassId = unitClassId;
+            AttackRate = attackRate;
+            AttackPoint = attackPoint;
+            AttackBackswing = attackBackswing;
+            ProjectileSpeed = projectileSpeed;
+            TurnRate = turnRate;
+        }
+
+        static void Main(string[] args)
+        {
+            Entity.OnIntegerPropertyChange += Entity_OnIntegerPropertyChange;
+        }
+
+        static void Entity_OnIntegerPropertyChange(Entity sender, EntityIntegerPropertyChangeEventArgs args)
+        {
+            if (args.Property == "m_nGameState")
+            {
+                if (!Game.IsInGame)
+                    return;
+            }
+
+            var me = EntityList.Hero;
+            var meData = AttackAnimationDatabase.GetByClassId(me.ClassId);
+            var gameTime = Game.GameTime;
+            var attackSpeed = Math.Max(me.AttackSpeed, 600);
+            var attackPoint = meData.AttackPoint / (1 + (attackSpeed - 100) / 100);
+            var attackRate = me.AttackBaseTime / (1 + (attackSpeed - 100) / 100);
+            if (sender != null && me.Equals(sender) && args.Property == "m_NetworkActivity")
+            {
+                if (args.NewValue == 424 || args.NewValue == 419)
+                    if (moveTime == 0)
+                    {
+                        moveTime = gameTime + attackPoint;
+                        endTime = gameTime + attackRate; 
+                    }
+                    else if (moveTime <= gameTime)
+                    {
+                        canMove = true;
+                    }
+                    else if (endTime <= gameTime)
+                    {
+                        canMove = false;
+                        moveTime = 0;
+                        endTime = 0;
+                    }
+                }
+            }
+        }
+
+        //static bool HaveModifier(Entity ent, String name)
+        //{
+        //    foreach (var modifier in ent.modifiers)
+        //    {
+        //        if (modifier.name == name)
+        //        {
+        //            return true
+        //        }
+        //    }
+        //    return false
+        //}
+
+    }
+}
---
+++ MoonesComboScript/AttackAnimationDatabase.cs
@@ -0,0 +1,178 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ensage;
+
+#endregion
+
+namespace MoonesComboScript
+{
+
+    public static class AttackAnimationDatabase
+    {
+        public static List<AttackAnimationData> Units = new List<AttackAnimationData>();
+
+        static AttackAnimationDatabase()
+        {
+            #region AntiMage
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_antimage",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_AntiMage,
+                    AttackRate = 1.45,
+                    AttackPoint = 0.3,
+                    AttackBackswing = 0.6,
+                    ProjectileSpeed = int.MaxValue,
+                    TurnRate = 0.5,
+                });
+
+            #endregion
+
+            #region Axe
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_axe",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_Axe,
+                    AttackRate = 1.7,
+                    AttackPoint = 0.5,
+                    AttackBackswing = 0.5,
+                    ProjectileSpeed = int.MaxValue,
+                    TurnRate = 0.6,
+                });
+
+            #endregion
+
+            #region Bane
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_bane",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_Bane,
+                    AttackRate = 1.7,
+                    AttackPoint = 0.3,
+                    AttackBackswing = 0.7,
+                    ProjectileSpeed = 900,
+                    TurnRate = 0.6,
+                });
+
+            #endregion
+
+            #region Bloodseeker
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_bloodseeker",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_Bloodseeker,
+                    AttackRate = 1.7,
+                    AttackPoint = 0.43,
+                    AttackBackswing = 0.74,
+                    ProjectileSpeed = int.MaxValue,
+                    TurnRate = 0.5,
+                });
+
+            #endregion
+
+            #region CrystalMaiden
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_crystal_maiden",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_CrystalMaiden,
+                    AttackRate = 1.7,
+                    AttackPoint = 0.55,
+                    AttackBackswing = 0,
+                    ProjectileSpeed = 900,
+                    TurnRate = 0.5,
+                });
+
+            #endregion
+
+            #region DrowRanger
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_drow_ranger",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_DrowRanger,
+                    AttackRate = 1.7,
+                    AttackPoint = 0.7,
+                    AttackBackswing = 0.3,
+                    ProjectileSpeed = 1250,
+                    TurnRate = 0.5,
+                });
+
+            #endregion
+
+            #region DrowRanger
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_drow_ranger",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_DrowRanger,
+                    AttackRate = 1.7,
+                    AttackPoint = 0.7,
+                    AttackBackswing = 0.3,
+                    ProjectileSpeed = 1250,
+                    TurnRate = 0.6,
+                });
+
+            #endregion
+
+            #region Earthshaker
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_earthshaker",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_Earthshaker,
+                    AttackRate = 1.7,
+                    AttackPoint = 0.467,
+                    AttackBackswing = 0.863,
+                    ProjectileSpeed = int.MaxValue,
+                    TurnRate = 0.6,
+                });
+
+            #endregion
+
+            #region Juggernaut
+
+            Units.Add(
+                new AttackAnimationData
+                {
+                    UnitName = "npc_dota_hero_juggernaut",
+                    UnitClassId = ClassId.CDOTA_Unit_Hero_Juggernaut,
+                    AttackRate = 1.6,
+                    AttackPoint = 0.33,
+                    AttackBackswing = 0.84,
+                    ProjectileSpeed = int.MaxValue,
+                    TurnRate = 0.6,
+                });
+
+            #endregion
+
+        }
+
+        public static AttackAnimationData GetByName(string unitName)
+        {
+            return Units.FirstOrDefault(unitData => unitData.UnitName.ToLower() == unitName);
+        }
+
+        public static AttackAnimationData GetByClassId(Ensage.ClassId classId)
+        {
+            return Units.FirstOrDefault(unitData => unitData.UnitClassId.Equals(classId));
+        }
+
+    }
+}
