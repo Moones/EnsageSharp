bddef37b5677be539255f4cbd07db2aefa68fe56
git <moones@email.cz>
2015-10-12 23:38:52 +0200

    Fixed Compilling Error

---
+++ Techies/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("#Techies")]
+[assembly: AssemblyDescription("Auto detonate on enemy heroes and creepwaves, auto ForceStaff into Remote Mines, auto KillSteal with Suicide Squad Attack. Idea by Zanko.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EnsageSharp")]
+[assembly: AssemblyProduct("#Techies")]
+[assembly: AssemblyCopyright("Copyright © Moones 2015")]
+[assembly: AssemblyTrademark("EnsageSharp")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("51a3de28-1c96-4567-4567-7380e0317dec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
