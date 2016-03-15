866532aa577bf58f915ee34486f6b7421a634139
git <moones@email.cz>
2016-03-15 17:21:05 +0100

    EntityNotFoundException fix

--- Ability/Ability/ObjectManager/Players/EnemyPlayers.cs
+++ Ability/Ability/ObjectManager/Players/EnemyPlayers.cs
@@ -33,7 +33,7 @@
                 return;
             }
 
-            if (All.Count < 5)
+            if (All.Count(x => x != null && x.Hero != null && x.IsValid) < 5)
             {
                 All =
                     Players.All.Where(
